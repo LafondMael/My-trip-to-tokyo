12b5fc8e140926a1eb881d5a4122c13b2ed1801f
Damien Mauger <mauger.damien@hotmail.fr>
2020-10-06 16:57:03 +0200

    modification de la page monuments et footer

--- script.js
+++ script.js
@@ -12,3 +12,20 @@ function show() {
 function close() {
   mainMenu.style.top = "-100%";
 }
+
+// Début partie read more
+let i = 0;
+function read(){
+  if(!i){
+    document.getElementById("more1").style.display = "inline";
+    document.getElementById("dots1").style.display = "none";
+    document.getElementById("read").innerHTML = "Read less";
+    i=1;
+  } else {
+    document.getElementById("more1").style.display = "none";
+    document.getElementById("dots1").style.display = "inline";
+    document.getElementById("read").innerHTML = "Read more";
+    i=0;
+  }
+}
+// Fin partie read more
