24505737df2b69d1c7f93e595473530c5b00a062
Damien Mauger <mauger.damien@hotmail.fr>
2020-10-07 13:18:25 +0200

    modify and update read more button

--- script.js
+++ script.js
@@ -14,18 +14,23 @@ function close() {
 }
 
 // Début partie read more
-let i = 0;
-function read(){
-  if(!i){
-    document.getElementById("more1").style.display = "inline";
-    document.getElementById("dots1").style.display = "none";
-    document.getElementById("read").innerHTML = "Read less";
-    i=1;
+
+const btn1 = document.querySelector(".button1");
+const btn2 = document.querySelector(".button2");
+const btn3 = document.querySelector(".button3");
+btn1.addEventListener("click", read);
+btn2.addEventListener("click", read);
+btn3.addEventListener("click", read);
+
+function read(e){
+  const p = e.target.parentElement;
+  const currentMore = p.querySelector(".more");
+  if (currentMore.style.display !== "inline"){
+    currentMore.style.display = "inline";
+    e.target.innerText = "Read Less";
   } else {
-    document.getElementById("more1").style.display = "none";
-    document.getElementById("dots1").style.display = "inline";
-    document.getElementById("read").innerHTML = "Read more";
-    i=0;
+    currentMore.style.display = "none";
+    e.target.innerText = "Read More";
   }
 }
 // Fin partie read more
